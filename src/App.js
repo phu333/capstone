0192b5709cf45bb85703e557e8b9a12c8cbf60ff
tran_tuyen <51820402+lamilaSS@users.noreply.github.com>
2020-10-20 11:34:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,29 +8,7 @@ function App() {
 
   return (
     <div >
-      <Particles
-        style={{ position: "absolute" }}
-        height="95%"
-        width="95%"
-        params={{
-          particles: {
-            color: {
-              value: "#000000"
-            },
-            line_linked: {
-              color: {
-                value: "#000000"
-              }
-            },
-            number: {
-              value: 50
-            },
-            size: {
-              value: 3
-            }
-          }
-        }}
-      />
+
       <LoginPage></LoginPage>
 
     </div>
