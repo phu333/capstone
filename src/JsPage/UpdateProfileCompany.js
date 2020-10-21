b8a2855ec8108718ea968b768b738e2a766923f1
tran_tuyen <51820402+lamilaSS@users.noreply.github.com>
2020-10-21 23:03:10 +0700

    Update UpdateProfileCompany.js

--- src/JsPage/UpdateProfileCompany.js
+++ src/JsPage/UpdateProfileCompany.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Popup from 'reactjs-popup';
 import 'antd/dist/antd.css';
 import '../index.css';
-
-import { Form, Input, Button, Checkbox } from 'antd';
+import { createEmployee, employeeInformation } from '../actions/EmployeeAction'
+import { connect } from 'react-redux'
+import { Space, Card, Button, Row, Col } from 'antd';
 import {
     IdcardOutlined, BankOutlined, HomeOutlined, MailOutlined
-    , CloudUploadOutlined, RedoOutlined
+    , CloudUploadOutlined, RedoOutlined, ReloadOutlined
 } from '@ant-design/icons';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import "./Column.css"
 const layout = {
     labelCol: {
         span: 4,
@@ -50,91 +58,132 @@ class UpdateProfileCompany extends React.Component {
     render() {
 
 
+        var i = 0;
+        function onChange(e) {
+            if (e.target == "checked") { i--; }
+            else { i++ }
+            console.log(`checked = ${e.target.checked}`);
+        }
+        function requirement() { if (i == 0) { } }
         return (
-            <div style={{
-                margin: "auto",
-                width: "1000px",
-                paddingLeft:"50px",
-                border:"solid"
-            }
-            }>
-                <Button type="primary" value="cancel" onClick={this.Cancel}>
-                        Trở về
+
+            <React.Fragment>
+                <br />
+                <h2 style={{ textAlign: 'center' }}>Thông tin cá nhân</h2>
+                <Space direction="horizontal" align="start"  >
+
+
+                    <Card style={{width:400, height:300}}>
+                        <Grid item xs={12}>
+
+                            <TextField
+                            placeholder="phu333"
+                                id="name"
+                                name="tên"
+                                label="phu333"
+                                fullWidth
+                                autoComplete="shipping address-level2"
+                                disabled
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+
+                            <TextField
+                                id="username"
+                                name="tên đăng nhập"
+                                label="phu333"
+                                fullWidth
+                                autoComplete="shipping address-level2"
+                                disabled
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="company"
+                                name="doanh nghiệp"
+                                label="HiSign"
+                                fullWidth
+                                autoComplete="shipping postal-code"
+                                disabled
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="role"
+                                name="chức vụ"
+                                label="Secretary"
+                                fullWidth
+                                autoComplete="shipping postal-code"
+                                disabled
+                            />
+                        </Grid>
+                    </Card>
+                    <Card style={{width:800, height:600}}>
+                        <Grid item xs={12}>
+
+                            <TextField
+                                id="password"
+                                name="mật khẩu"
+                                label="mật khẩu"
+                                fullWidth
+                                autoComplete="shipping address-level2"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+
+                            <TextField
+                                id="email"
+                                name="địa chỉ mail"
+                                label="địa chỉ mail"
+                                fullWidth
+                                autoComplete="shipping address-level2"
+                            />
+                        </Grid>
+                        <Grid item xs={12} gutter={16}>
+                            <TextField
+                                id="address"
+                                name="địa chỉ"
+                                label="địa chỉ"
+                                fullWidth
+                                autoComplete="shipping address-line2"
+                                
+                            />
+                        </Grid>
+                        <div style={{marginTop:"50px", paddingLeft:"200px"}}>
+                        <Space size="large">
+                            <Popup trigger={<Button type="primary" >
+                                <CloudUploadOutlined />  Nộp
+                            </Button>} position="right center">
+                                <div>The sytem admin will contact you within 3 days</div>
+                                <Button onClick={this.onFinish} type="primary" >
+                                    Xác nhận
+                            </Button>
+                            </Popup>
+
+                            <Button type="primary" style={{
+                                margin: '0 8px',
+                            }} htmlType="button">
+                                <ReloadOutlined />   Reset
+                            </Button>
+                            <Button type="primary" value="cancel" onClick={this.Cancel}>
+                                Trở về
               </Button>
-                <h2 style={{ textAlign: 'center' }}>Thông tin doanh nghiệp</h2>
-                <Form
-                    {...layout}
-                    name="basic"
-                    initialValues={{
-                        remember: true,
-                    }}
-                    labelAlign="left"
-                    onFinish={this.onFinish}
-                    onFinishFailed={this.onFinishFailed}
-                ><br />
-                    <Form.Item
-                        label={(<b><IdcardOutlined />{"Tên doanh nghiệp"} </b>)}
-                        name="companyname"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Vui lòng nhập tên doanh nghiệp',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
+                        </Space>
+                        </div>
+                    </Card>
 
-                    <Form.Item
-                        label={(<b><MailOutlined /> {"Địa chỉ Email"} </b>)}
-                        name="email"
-                        requiredMark="false"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Vui lòng nhập Địa chỉ email',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        label={(<b><BankOutlined /> {"Mã số thuế"} </b>)}
-                        name="taxcode"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Vui lòng nhập Mã số thuế',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        label={(<b><HomeOutlined /> {"Địa chỉ"} </b>)}
-                        name="address"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Vui lòng nhập Địa chỉ',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
 
-                    <Form.Item {...tailLayout}>
-                        <Button type="primary" htmlType="submit">
-                            <CloudUploadOutlined /> Nộp
-                            </Button>
-                        <Button type="primary" style={{
-                            margin: '0 8px',
-                        }} htmlType="button">
-                            <RedoOutlined />    Reset
-                            </Button>
-                    </Form.Item>
-                </Form>
-            </div >
+
+
+
+
+
+
+
+                </Space>
+            </React.Fragment >
         );
     }
 
