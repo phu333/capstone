cd1a80c0c1805c721df9703af1753b529c43a0a8
phu223 <phuNNSE62953@fpt.edu.vn>
2020-10-23 21:37:04 +0700

    zxczxc

--- src/JsPage/Update/UpdateProfileCompany.js
+++ src/JsPage/Update/UpdateProfileCompany.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
-import Chart from '../Table/RevenueChart';
+import Chart from './RevenueChart';
 import {
     IdcardOutlined, BankOutlined, HomeOutlined, MailOutlined
     , CloudUploadOutlined, RedoOutlined
