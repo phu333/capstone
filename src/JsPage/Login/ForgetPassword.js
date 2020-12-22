29391065e8ee69532261d6551ed52569ddcd9bb5
NBao97 <trauvangmayman97@gmail.com>
2020-12-22 23:51:51 +0700

    update layout Login and app

--- src/JsPage/Login/ForgetPassword.js
+++ src/JsPage/Login/ForgetPassword.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { BrowserRouter as Router, Route, Switch, Redirect, useHistory } from 'react-router-dom'
-import { Form, Input, Button, Checkbox,Col,Row } from 'antd';
+import { Form, Input, Button, Card, Col, Row } from 'antd';
 
 const layout = {
     labelCol: {
@@ -48,43 +48,53 @@ class ForgetPassword extends React.Component {
 
 
         return (
-            <Row type="flex" justify="center" align="middle" style={{ height: "100vh" }}>
+<Card style={{ height: "100vh", backgroundColor: 'rgb(8, 59, 102)' }}>
+                    <Button style={{ width: '80px' }} type="primary" value="cancel" onClick={this.Cancel}>
+                        Trở về
+              </Button>
+                    <br />
+                    <br />
+            <Row type="flex" justify="center" align="middle" >
+
                 <Col span={10} >
-            <Form
-                {...layout}
-                name="basic"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={this.onFinish}
-                onFinishFailed={this.onFinishFailed}
-                
-            >
-                <Form.Item
-                    label="Địa chỉ Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Vui lòng nhập email',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
 
-               
+                    <Form
+                        {...layout}
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        hideRequiredMark
+                        onFinish={this.onFinish}
+                        onFinishFailed={this.onFinishFailed}
+
+                    >
+                        <Form.Item
+                            label={<label style={{ color: "White" }}>Địa chỉ Email</label>}
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+
+                                    message: <label style={{ backgroundColor: "White" }}>Vui lòng nhập email</label>,
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
 
 
 
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                        Nộp
+
+
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                                Nộp
                             </Button>
-                    
-                </Form.Item>
 
-            </Form></Col></Row>
+                        </Form.Item>
+
+                    </Form></Col></Row></Card>
         );
     }
 
