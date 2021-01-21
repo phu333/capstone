081d15c4c1bb582398685c7d381b1d46a6a9f3f9
BaoNHN197 <trauvangmayman97@gmail.com>
2021-01-21 15:56:25 +0700

    minor update

--- src/JsPage/Update/ViewCustomer.js
+++ src/JsPage/Update/ViewCustomer.js
@@ -202,7 +202,7 @@ class ViewCustomer extends React.Component {
                     <Form.Item {...tailLayout}>
                         <Space size="large">
                             {this.state.isEdit === true ? <Button type="primary" htmlType="submit" className="login-form-button">
-                                Tạo
+                                Sửa
                             </Button> : null}
                             {this.state.isEdit === true ? <Button type="primary" htmlType="reset" className="login-form-button">
                                 Reset
